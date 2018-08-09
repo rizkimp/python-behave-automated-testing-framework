769a38eda1e69aaad69352a70e883684590475f3
Rizki Maha Putra <30515602+rizkimp@users.noreply.github.com>
2018-08-09 12:57:53 +0700

    Update login_and_logout.py

--- features/steps/login_and_logout.py
+++ features/steps/login_and_logout.py
@@ -11,12 +11,12 @@ def step_impl(context):
     context.browser.find_element(By.XPATH,locator.landing)
     context.browser.find_element(By.XPATH,locator.slide)
 
-@when(u'login using username : rizkimahaputra & password : rizkimahaputra')
+@when(u'login using username : richard & password : 12345678')
 def step_impl(context):
     context.browser.find_element(By.XPATH,locator.button_login1).click()
     context.browser.find_element(By.XPATH,locator.form_login)
-    context.browser.find_element(By.XPATH,locator.form_username).send_keys('rizkimahaputra')
-    context.browser.find_element(By.XPATH,locator.form_password).send_keys('rizkimahaputra')
+    context.browser.find_element(By.XPATH,locator.form_username).send_keys('richard')
+    context.browser.find_element(By.XPATH,locator.form_password).send_keys('12345678')
     context.browser.find_element(By.XPATH,locator.button_login2).click()
 
 @then(u'direct to homepage users')
